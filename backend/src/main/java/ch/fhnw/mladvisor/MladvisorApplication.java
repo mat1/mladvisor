362e212f6ba3440bb5f5d56de1c0873cec5eb7d8
Matthias Brun <matthias.brun@ds-i.ch>
2018-10-07 09:23:18 +0200

    Inital commit

---
+++ backend/src/main/java/ch/fhnw/mladvisor/MladvisorApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.mladvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MladvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MladvisorApplication.class, args);
+	}
+}
