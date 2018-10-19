4b9906a1d4162eabc5fc7db7305de99aa5b2c2cb
Matthias Brun <matthias.brun@ds-i.ch>
2018-10-19 21:14:52 +0200

    Restore answer

--- ui/src/survey.js
+++ ui/src/survey.js
@@ -8,31 +8,31 @@ class Criterion {
 }
 
 class TextQuestion {
-  constructor(id, category, question) {
+  constructor(id, category, question, answer) {
     this.id = id;
     this.category = category;
     this.question = question;
-    this.answer = null;
+    this.answer = answer;
   }
 }
 
 class RadioQuestion {
-  constructor(id, category, question, answers) {
+  constructor(id, category, question, answers, answer) {
     this.id = id;
     this.category = category;
     this.question = question;
     this.answers = answers;
-    this.answer = null;
+    this.answer = answer;
   }
 }
 
 const toQuestions = questions => {
   return questions.map(q => {
     if (q.type == "TextQuestion") {
-      return new TextQuestion(q.id, q.category, q.question);
+      return new TextQuestion(q.id, q.category, q.question, q.answer);
     }
     if (q.type == "RadioQuestion") {
-      return new RadioQuestion(q.id, q.category, q.question, q.answers);
+      return new RadioQuestion(q.id, q.category, q.question, q.answers, q.answer);
     }
     if (q.type == "Criterion") {
       return new Criterion(q.id, q.category, q.question, q.answer);
