c2c6445733e51b1ee72f99b013b25fe4251b2270
Matthias Brun <matthias.brun@ds-i.ch>
2018-10-19 16:04:38 +0200

    Fix link for IE

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -3,7 +3,9 @@
     <div class="row">
       <div class="col s12">
         <h4>ML Advisor</h4>
-        <a class="waves-effect waves-light btn-large" href="#/survey">Start</a>
+        <router-link to="/survey">
+          <a class="waves-effect waves-light btn-large">Start</a>
+        </router-link>
       </div>
     </div>
   </div>
