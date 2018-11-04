dcd70842e3c78333eacfc5eff1d11639c164cc43
Matthias Brun <matthias.brun@ds-i.ch>
2018-11-04 14:06:18 +0100

    Add template for workshop and flipchart

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -3,13 +3,35 @@
     <div class="row">
       <div class="col s12">
         <h4>ML Advisor</h4>
-        <p>Machine Learning (ML) Advisor helps you and your company to identify if your use case is suitable 
+        <p>Machine Learning (ML) Advisor helps you and your company to identify if your use case is suitable
           for machine learning and to compare different use cases with each other. Press start and answer some questions about your case.</p>
         <button class="waves-effect waves-light btn-large" @click="start">Start</button>
-        <p>You are not at the point where you already identified potential use cases for machine learning? 
+        <p>You are not at the point where you already identified potential use cases for machine learning?
           You are not sure which potential machine learning for your company has? Then the identify use cases for machine learning workshop is something for you.</p>
-        <h5>Identify Use Cases for Machine Learning Workshop</h5>
       </div>
+
+      <div class="row">
+        <div class="col s12">
+          <h5>Identify Use Cases for Machine Learning Workshop</h5>
+          <p>The ml advisor process supports your business to get a basic understanding about machine learning and to identify suitable cases.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          <img ref="materialbox" class="responsive-img" src="../assets/advisor-process.png" alt="ML Advisor Process" />
+          <p class="center-align">ML Advisor Process</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          <p>The workshop and the flipchart template which is used in the workshop can be downloaded with the link below.</p>
+        </div>
+        <div class="col s12">
+          <a class="waves-effect waves-light btn" href="/templates/Identify-Use-Cases-for-Machine-Learning-Workshop.pdf" target="_blank">Download Workshop</a>
+          <a class="waves-effect waves-light btn" style="margin-left:10px" href="/templates/Flipchart-Template.pdf" target="_blank">Flipchart Template</a>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -19,6 +41,9 @@ import Api from "@/api";
 
 export default {
   name: "home",
+  mounted() {
+    var instances = M.Materialbox.init(this.$refs.materialbox, {});
+  },
   methods: {
     start() {
       Api.startSurvey().then(survey => {
