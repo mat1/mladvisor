14c2a59cae104d056ac5c450e772f3f066ade56f
Matthias Brun <matthias.brun@ds-i.ch>
2018-11-04 11:19:10 +0100

    Fix typo

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -6,7 +6,7 @@
         <p>Machine Learning (ML) Advisor helps you and your company to identify if your use case is suitable 
           for machine learning and to compare different use cases with each other. Press start and answer some questions about your case.</p>
         <button class="waves-effect waves-light btn-large" @click="start">Start</button>
-        <p>You are not at the point where your already identified potential use cases for machine learning? 
+        <p>You are not at the point where you already identified potential use cases for machine learning? 
           You are not sure which potential machine learning for your company has? Then the identify use cases for machine learning workshop is something for you.</p>
         <h5>Identify Use Cases for Machine Learning Workshop</h5>
       </div>
