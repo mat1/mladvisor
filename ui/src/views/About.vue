54fc27936aee929bdc169ec2015cdb1930e9cbaa
Matthias Brun <matthias.brun@ds-i.ch>
2018-12-15 09:00:53 +0100

    Update About page

--- ui/src/views/About.vue
+++ ui/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h4>About</h4>
-    <p>ML Advisor was developed as Master Thesis at University of Applied Sciences and Arts Northwestern Switzerland</p>
+    <p>ML Advisor was developed as part of a Master Thesis at University of Applied Sciences and Arts Northwestern Switzerland</p>
   </div>
 </template>
